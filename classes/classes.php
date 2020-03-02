289ba57fb83eff091b74a2f5161894c0c07d3689
alelima07 <57467196+alelima07@users.noreply.github.com>
2020-03-01 22:31:06 -0800

    Add files via upload

---
+++ classes/classes.php
@@ -0,0 +1,81 @@
+<?php
+class Calculator {
+    public $num1 = 0;
+    public $num2 = 0;
+    public $result = 0;
+    public $operator = "";
+
+    function add(){
+        $this->result = $this->num1 + $this->num2;
+        return $this->result; 
+    }
+
+    function subtract(){
+        $this->result = $this->num1 - $this->num2;
+        return $this->result;   
+    }
+
+    function divide(){
+        $this->result = $this->num1 / $this->num2;
+        return $this->result;   
+    }
+
+    function multiply(){
+        $this->result = $this->num1 * $this->num2;
+        return $this->result; 
+    }
+}
+?>
+
+<html>
+    <head>
+        <title>Classes PHP</title>
+        <link rel="stylesheet" type="text/css" href="classes.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Poppins&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        <h1>Seattle Central College</h1>
+        <h1>ITC 240 - PHP</h1>
+        <h1>Classes</h1>
+        <p>Assignment: Write a class to represent a basic artithmetic operations calculator.</p>
+        <p>Your class should have the ability to add, subtract, multiply or divide two numbers.</p>
+        <h2>Calculator</h2>
+        <p>Feel free to use this calculator for basic math.</p>
+        <form method="POST">
+            <input type="number" name="num1">
+            <select name="operations">
+                <option value="">Select operator</option>
+                <option value="add">PLUS(+)</option>
+                <option value="sub">MINUS(-)</option>
+                <option value="mult">TIMES(*)</option>
+                <option value="div">DIVIDED BY(/)</option>
+            </select>
+            <input type="number" name="num2">
+            <input type="submit" value="CALCULATE">
+        </form>
+        <?php
+        $calc = new Calculator;
+        $calc->num1 = $_POST['num1'];
+        $calc->num2 = $_POST['num2'];
+
+        if(isset($_POST['operations'])){
+            if($_POST['operations'] == 'add'){
+                $calc->result = $calc->add();
+                $calc->operator = "+";
+            } elseif($_POST['operations'] == 'sub'){
+                $calc->result = $calc->subtract();
+                $calc->operator = "-";
+            } elseif($_POST['operations'] == 'mult'){
+                $calc->result = $calc->multiply();
+                $calc->operator = "*";
+            } elseif($_POST['operations'] == 'div') {
+                $calc->result = $calc->divide();
+                $calc->operator = "/";
+            } else {
+                $calc->result = "Please select an operator.";
+            }
+            echo "<p>" . $calc->num1 . " " . $calc->operator. " " . $calc->num2 . " = " . $calc->result . "</p>";
+        } 
+        ?>
+    </body>
+</html>
